f01309d8cd9acbd0bf25bd568295ee18a62a32dd
Teemo Tebest <teemo.tebest@gmail.com>
2025-02-06 10:48:25 +0100

    Added content

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import Footer from './jsx/Footer.jsx';
+import FooterEN from './jsx/FooterEN.jsx';
 
-const container_footer = document.getElementById('app-root-2024-edar_report_footer');
-const root_footer = createRoot(container_footer);
-root_footer.render(<Footer />);
+import FooterFR from './jsx/FooterFR.jsx';
+
+const container_footer_en = document.getElementById('app-root-2024-edar_report_footer_en');
+if (container_footer_en) {
+  const root_footer_en = createRoot(container_footer_en);
+  root_footer_en.render(<FooterEN />);
+}
+
+const container_footer_fr = document.getElementById('app-root-2024-edar_report_footer_fr');
+if (container_footer_fr) {
+  const root_footer_fr = createRoot(container_footer_fr);
+  root_footer_fr.render(<FooterFR />);
+}
